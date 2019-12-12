54b282fef7f7a534b43224da57db04fda288b189
xingchen <dxc1907494206@outlook.com>
2019-12-12 09:59:22 +0800

    自己实现了哈希表查找的链地址法

---
+++ Z14折半查找和哈希表查找/Z14折半查找和哈希表查找.cpp
@@ -0,0 +1,63 @@
+﻿// Z14折半查找和哈希表查找.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+const int M = 100;
+int x, n, i;
+int s[M];
+
+int BinarySearch(int s[], int n, int x)//---二分查找非递归
+{
+	int low = 0, hign = n - 1;
+	while (low <= hign)
+	{
+		int middle = low + (hign - low) / 2;
+		if (x == s[middle])
+			return middle;
+		else if (x > s[middle])
+			low = middle + 1;
+		else
+			hign = middle - 1;
+	}
+	return -1;
+}
+int recursionBS(int s[], int x, int low, int high)//---二分查找递归
+{
+	if (low > high)
+		return -1;
+	int middle = low + (high - low) / 2;
+	if (x == s[middle])
+		return middle;
+	else if (x < s[middle])
+		return recursionBS(s, x, low, middle - 1);
+	else
+		return recursionBS(s, x, middle + 1, high);
+}
+int main()
+{
+	std::cout << "Hello World!\n";
+	cout << "该数列中的元素个数n为：";
+	cin >> n;
+	cout << "请依次输入数列中的元素：";
+	for (i = 0; i < n; i++)
+		cin >> s[i];
+	sort(s, s + n); //二分查找的序列必须是有序的，如果无序需要先排序
+	cout << "排序后的数组为：";
+	for (i = 0; i < n; i++)
+	{
+		cout << s[i] << " ";
+	}
+	cout << endl;
+	cout << "请输入要查找的元素：";
+	cin >> x;
+	//i = BinarySearch(s, n, x);
+	i = recursionBS(s, x, 0, n - 1);
+	if (i == -1)
+		cout << "该数列中没有要查找的元素" << endl;
+	else
+		cout << "要查找的元素在第" << i + 1 << "位" << endl;//位序和下标差1
+	return 0;
+}
---
+++ Z15哈希表查找/Z15哈希表查找.cpp
@@ -0,0 +1,122 @@
+﻿// Z15哈希表查找.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//---链地址法又叫拉链法
+
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+#define  M 15
+#define  NULLKey 0
+
+typedef struct OVnode
+{
+	int v;
+	struct  OVnode* next;
+}OVnode;
+typedef struct Hnode
+{
+	int data;
+	OVnode* next;
+}Hnode;
+typedef struct
+{
+	Hnode Hash[M];
+}HashTable;
+void InitH(HashTable& H)
+{
+	for (int i = 0; i < M; i++)
+	{
+		H.Hash[i].data = NULLKey;
+		H.Hash[i].next = NULL;
+	}
+}
+int HK(int key)
+{
+	return key % 13;
+}
+bool InsertHash(HashTable& H, int key)
+{
+	int h0 = HK(key);
+	if (H.Hash[h0].data == NULLKey)
+	{
+		H.Hash[h0].data = key;
+		return true;
+	}
+	else
+	{
+		OVnode* h = new OVnode;
+		h->v = key;
+		h->next = H.Hash[h0].next;
+		H.Hash[h0].next = h;
+		return true;
+	}
+	return false;
+}
+int SearchHash(HashTable H, int key)
+{
+	int h0 = HK(key); int x = 1;
+	if (H.Hash[h0].data == NULLKey)
+		return -1;
+	else if (H.Hash[h0].data == key)
+		return x;
+	else
+	{
+		OVnode* p;
+		p = H.Hash[h0].next;
+		while (p != NULL)
+		{
+			x++;
+			if (p->v == key)
+				return x;
+			p = p->next;
+		}
+		return -1;
+	}
+}
+void PrintH(HashTable H)
+{
+	for (int i = 0; i < M; i++)
+	{
+		if (H.Hash[i].data != NULLKey)
+		{
+			cout << "第" << i + 1 << "位：" << H.Hash[i].data << "  ";
+
+			OVnode* p = H.Hash[i].next;
+			while (p != NULL)
+			{
+				cout << p->v << "  ";
+				p = p->next;
+			}
+			cout << endl;
+		}
+	}
+}
+int main()
+{
+	std::cout << "Hello World!\n";
+	HashTable H; int x;
+
+	InitH(H);
+	cout << "输入12个关键字，存入哈希表中：" << endl;
+	for (int i = 0; i < 12; i++)
+	{
+		cin >> x;
+		if (!InsertHash(H, x))
+		{
+			cout << "创建哈希表失败！" << endl;
+			return 0;
+		}
+	}
+	cout << "输出哈希表：" << endl;
+	PrintH(H);
+	cout << "输入要查找的关键字" << endl;
+	cin >> x;
+	int y = HK(x);
+	int result = SearchHash(H, x);
+	if (result != -1)
+		cout << "在第" << y + 1 << "-" << result << "位置找到" << endl;
+	else
+		cout << "未找到" << endl;
+	return 0;
+}
